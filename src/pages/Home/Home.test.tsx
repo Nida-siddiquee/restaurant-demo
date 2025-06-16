8df8764094591bfa21d3181fa0cbbcedb02bdc43
nida <nidasiddiquee204@gmail.com>
2025-06-16 13:14:33 +0200

    fixes

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,6 +1,4 @@
-// src/pages/Home/Home.test.tsx
 
-import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { MemoryRouter } from 'react-router-dom';
